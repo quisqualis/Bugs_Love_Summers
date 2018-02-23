8487b2a61ff5104f06dbb295b5675bd942d80467
Sanjay <sanmishr@gmail.com>
2018-02-22 21:27:14 -0600

    2-22-18

--- app.R
+++ app.R
@@ -1,48 +1,62 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+
+  # Some custom CSS for a smaller font for preformatted text
+  tags$head(
+    tags$style(HTML("
+      pre, table.table {
+        font-size: smaller;
+      }
+    "))
+  ),
+  
+  # Application title
+  titlePanel("Heating and Cooling Days in USA"),
+  
+  # Creates a sidebar panel containing input controls 
+  sidebarPanel(
+          helpText("Explore the changing climate in USA"),
+          
+     # select data for Y axis:
+  selectInput("var", 
+              label = "Choose a variable to display",
+              choices = c("Heating Days" = 'heating_days', 
+                          "Cooling Days" = "cooling_days"),
+              selected = "Heating Days")
+    ),
+  
+  # provide x-axis
+  # selectInput(inputId = "year",
+  #             label = "Year",
+  #             choices = c("year"), 
+  #             selected = ""),
+  
+
+  # Outputs
+  mainPanel(
+    plotOutput(outputId = "scatterplot")
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw the scatterplot
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  # Create scatterplot object the plotOutput function is expecting
+  output$scatterplot <- renderPlot({
+    ggplot(data = heating_cooling_degree_days, 
+           aes_string(x = heating_cooling_degree_days$year, y = input$var)) +
+          geom_point(color= ifelse (input$var == "heating_days", "red", "blue"), shape = 1, alpha = 1) +
+      geom_smooth(color= ifelse (input$var == "heating_days", "red", "blue")) +
+      xlab("Year") + 
+      ylab(ifelse (input$var == "heating_days", "Heating Days", "Cooling Days")) +
+      labs(title = "Heating and Cooling Degree Days in the Contiguous 48 States, 1895–2015", 
+           caption = "Data source: NOAA, 2016") +
+      theme_bw()
+  })
+ 
 }
 
 # Run the application 
