bde2ba11b1ae9ce087f0e119d6f77aac04b484c4
Sanjay <sanmishr@gmail.com>
2018-02-27 21:26:16 -0600

    2-27-18

--- server.R
+++ server.R
@@ -3,8 +3,21 @@ library(ggplot2)
 
 load(file = "data/data.RData")
 
+
+# custom ggplot theme to drop the axes and ticks but leave the guides and legends
+ditch_the_axes <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank(),
+  panel.background = element_blank()
+)
+
 # Define server logic required to draw the scatterplot
 server <- function(input, output) {
+
   
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
@@ -14,9 +27,40 @@ server <- function(input, output) {
       geom_smooth(color= ifelse (input$var == "heating_days", "red", "blue")) +
       xlab("Year") + 
       ylab(ifelse (input$var == "heating_days", "Heating Days", "Cooling Days")) +
-      labs(title = "Heating and Cooling Degree Days in the Contiguous 48 States, 1895–2015", 
+      labs(title = "Heating and Cooling Degree Days in lower 48", 
+           subtitle = "(1895–2015)",
            caption = "Data source: NOAA, 2016") +
-      theme_bw()
+      theme(legend.title = element_text(size = 10),
+            legend.text = element_text(size = 12),
+            plot.title = element_text(size=20),
+            axis.title=element_text(size=14,face="bold"),
+            axis.text.x = element_text(angle = 0, hjust = 1)) +
+      theme_classic()
   })
   
+  
+        output$US_map <- renderPlot({
+          
+        # generating data for US_map
+        Lyme_map_data <-  Lyme_State %>% 
+          select(region, cases = as.character(input$year)) %>% 
+          mutate(region = tolower(region))
+        Lyme_map_data <- 
+          merge(us_state_map, Lyme_map_data, by='region', all=T)
+        
+        ggplot(data = Lyme_map_data, aes(x=long, y=lat, group=group, fill=cases)) +
+          scale_fill_gradientn("",colours=brewer.pal(n=4, "YlOrRd"))+
+          geom_polygon()+coord_map()+
+          labs(title=paste(input$year,"Lyme disease incidence rates by state"),
+                           subtitle="(confirmed cases per 100,000 persons)",
+               caption = "Data source: CDC, 2016",
+               fill = "Incidence")+
+          theme(legend.title = element_text(size = 10),
+                legend.text = element_text(size = 12),
+                plot.title = element_text(size=20)
+                ) +
+          ditch_the_axes
+
+    }
+  )
 }
--- ui.R
+++ ui.R
@@ -6,6 +6,15 @@ load(file = "data/data.RData")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
+  
+  tags$style(HTML(".irs-bar {width: 100%; height: 15px; background: black; border-top: 1px solid black; border-bottom: 1px solid black;}")),
+  tags$style(HTML(".irs-bar-edge {background: black; border: 1px solid black; height: 15px; border-radius: 5px 5px 5px 5px;}")),
+  tags$style(HTML(".irs-line {border: 1px solid black; height: 15px;}")),
+  tags$style(HTML(".irs-grid-text {font-family: 'arial'; color: black}")),
+  tags$style(HTML(".irs-max {font-family: 'arial'; color: black;}")),
+  tags$style(HTML(".irs-min {font-family: 'arial'; color: black;}")),
+  tags$style(HTML(".irs-single {color:black; background:#6666ff;}")), 
+  
   # Some custom CSS for a smaller font for preformatted text
   tags$head(
     tags$style(HTML("
@@ -16,31 +25,39 @@ ui <- fluidPage(
   ),
   
   # Application title
-  titlePanel("Climate Change Indicators"),
+  titlePanel("Climate Change Indicators"), 
   
-  # adding the description to the sidebar            
-  tags$div(class="header", checked=NA,
-           tags$a(href="https://www.epa.gov/climate-indicators/climate-change-indicators-heating-and-cooling-degree-days", "description by EPA")
-  ),
-  
-  
+  sidebarLayout(position = "left",
   # Creates a sidebar panel containing input controls 
-  sidebarPanel(
-          helpText("Explore the changing climate in USA"),
-          
-       # select data for Y axis:
+  sidebarPanel("",
+  
+  
+  helpText("Explore the changing climate in USA"),
+  
+  # select data for Y axis:
   selectInput("var", 
               label = "Choose a variable to display",
               choices = c("Heating Days" = 'heating_days', 
                           "Cooling Days" = "cooling_days"),
-              selected = "Heating Days")
-    ),
+              selected = "Heating Days"),
   
-  selectInput("year", 
+  
+  
+  # selectInput("year",
+  #             label = "Choose a year",
+  #             choices = c("2016","2015","2014","2013","2012",
+  #                         "2011","2010","2009","2008","2007","2006"),
+  #             selected = "2016"),
+  
+  
+  sliderInput("year",
+              sep="",
               label = "Choose a year",
-              choices = c("2016","2015","2014","2013","2012",
-                          "2011","2010","2009","2008","2007","2006"),
-              selected = "2016"),
+              min = 2006,
+              max = 2016,
+              value = 2016,
+              step = 1, ticks = FALSE,
+              width = "100%"),
   
   # provide x-axis
   # selectInput(inputId = "year",
@@ -48,24 +65,31 @@ ui <- fluidPage(
   #             choices = c("year"), 
   #             selected = ""),
   
-
-  # Outputs
-  mainPanel(
-    
-    #  Desctest
-    
-    p("A “degree day” is determined by comparing the daily average outdoor temperature 
-      with a defined baseline temperature for indoor comfort (in this case, 65°F). 
-      If the average temperature on a particular day is 78°F, then that day counts as 13 cooling degree days, as a building’s interior would need to be cooled by 13°F to reach 65°F. 
-Conversely, if the average outdoor temperature is 34°F, then that day counts as 31 heating degree days, as a building’s interior would need to be warmed by 31°F to reach 65°F. 
-A nationally applied baseline—in this case, 65°F—has certain limitations 
-      considering the various climate regimes across the United States."),
-    style = "font-family: 'Arial'; font-si16pt"),
-    em(strong("Degree Days are indicators of climate change")),
-    
-    
-    plotOutput(outputId = "scatterplot"),
+  width = 2
+  ), 
   
-  plotOutput(outputId = "US_map")
+  
+  # Outputs
+  mainPanel("",          
+            fluidRow(
+              
+              #     #  Description text
+              #     
+              #     p("A “degree day” is determined by comparing the daily average outdoor temperature 
+              #       with a defined baseline temperature for indoor comfort (in this case, 65°F). 
+              #       If the average temperature on a particular day is 78°F, then that day counts as 13 cooling degree days, as a building’s interior would need to be cooled by 13°F to reach 65°F. 
+              # Conversely, if the average outdoor temperature is 34°F, then that day counts as 31 heating degree days, as a building’s interior would need to be warmed by 31°F to reach 65°F. 
+              # A nationally applied baseline—in this case, 65°F—has certain limitations 
+              #       considering the various climate regimes across the United States."),
+              #     style = "font-family: 'Arial'; font-si16pt"),
+              #     em(strong("Degree Days are indicators of climate change")),
+              
+              splitLayout(style = "border: 1px solid silver:", cellWidths = c(400,800),
+                          plotOutput(outputId = "scatterplot"),
+                          plotOutput(outputId = "US_map")
+              )
+            )
+            )
+)
 )
 
